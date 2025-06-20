916b2cdc541165a0e95e1bf4b84f7338ae346adf
naroalaria <naroa.laria@alumni.mondragon.edu>
2025-06-20 10:34:25 +0200

    la api

---
+++ api.R
@@ -0,0 +1,15 @@
+release_year <- as.numeric(datos$release_year)
+apiexamen <- "
+#* @apiTitle Plumber Example API
+#* @apiDescription Plumber  description
+
+#* @param release_year
+#* @get /echo
+function(release_year) {
+    return()
+}"
+
+writeLines(apiexamen,"api.R")
+library(plumber)
+r <- plumb("api.R")
+r$run(port=8000)
